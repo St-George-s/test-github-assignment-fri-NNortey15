995494018d69dce50588973ade107efdeaefbc6e
NNortey15 <nor107@stge.org.uk>
2024-08-30 09:09:40 +0000

    I am have completed task 1

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-for x in range(1000000):
-    print(str(x) + " x " + str(13.5) + " = " + str(x * 13.5))
+def get_destination():
+    destination= input("Enter the travel destination (type 'END' to stop): ")
+    return destination 
+
+def getNumberofPeople():
+    numberOfpeople= input("Enter the amount of people going on the trip:")
+    return numberOfpeople
+
+def getTravelmethod():
+    travelMethod= input("What method of travel do you use?")
+    return travelMethod
+
+def print_travel_details(destination, numberOfpeople, travelMethod):
+    print(destination + numberOfpeople + travelMethod)
+
+#MAIN PROGRAM
+destination=get_destination()   
+while destination != "END":
+    numberOfpeople=getNumberofPeople()
+    travelMethod=getTravelmethod()
+
+    print( "Your destination is" + destination + "and you plan on getting there by "+ travelMethod + "with " + numberOfpeople + "people")
+    destination=get_destination()   
+print("Bye")
