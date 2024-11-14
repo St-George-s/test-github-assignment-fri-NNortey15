11af75138460cae1c07d3a4e69bb6abfeee2a0a5
NNortey15 <nor107@stge.org.uk>
2024-11-14 14:21:18 +0000

    Finish Photography worksheet

--- Photography/main.sql
+++ Photography/main.sql
@@ -1,1 +1,36 @@
-DESCRIBE ALL TABLES;
+-- SELECT P.photographer_id, name
+-- FROM Bookings B
+-- JOIN Photographers P ON P.photographer_id = B.photographer_id
+-- WHERE date_booked  = (
+--     SELECT MIN(date_booked )
+--     FROM Bookings);
+
+-- SELECT name
+-- FROM Clients
+-- WHERE client_id  =(SELECT event_type ="Wedding"
+-- FROM Bookings);
+
+-- SELECT name
+-- FROM Clients
+-- WHERE client_id  IN (
+--     SELECT client_id
+--     FROM Bookings
+--     WHERE event_type ="Wedding"
+-- );
+
+--  SELECT client_id
+--     FROM Bookings
+--     WHERE event_type ="Wedding";
+
+-- Outer and inner query.Inner query gets evaluated first 
+SELECT photographer_id
+FROM Photographers
+WHERE experience_years > 5;
+
+SELECT COUNT(*)
+FROM Photos
+WHERE photographer_id IN (
+    SELECT photographer_id
+    FROM Photographers
+    WHERE experience_years > 5
+);
