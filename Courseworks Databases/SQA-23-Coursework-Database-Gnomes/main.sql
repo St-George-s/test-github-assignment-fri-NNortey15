e7de136af69f421bb70145280d9395fe39b97279
NNortey15 <nor107@stge.org.uk>
2024-11-21 14:23:27 +0000

    still doing 2023 Coursework

--- Courseworks Databases/SQA-23-Coursework-Database-Gnomes/main.sql
+++ Courseworks Databases/SQA-23-Coursework-Database-Gnomes/main.sql
@@ -1,1 +1,10 @@
-DESCRIBE ALL TABLES;
+SELECT gnomeName, SUM(quantity) 
+FROM Gnome G,Gnomepurchase P  
+WHERE G.description LIKE "%solar%" 
+and G.gnomeid = P.gnomeid 
+GROUP BY gnomeName 
+ORDER BY sum(quantity) DESC; 
+
+SELECT gnomeID
+FROM gnome
+WHERE 
