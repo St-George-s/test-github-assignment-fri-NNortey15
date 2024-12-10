56fd4a0a7dcd218a102a3abebfc77fa8ef4f5661
NNortey15 <nor107@stge.org.uk>
2024-12-10 09:15:40 +0000

    Shop mock cw

--- Database - Database Test/main.sql
+++ Database - Database Test/main.sql
@@ -1,1 +1,21 @@
 DESCRIBE ALL TABLES;
+
+-- SELECT episodeTitle, showName , maxViewers, episodedate
+-- FROM show S ,Episode E 
+-- WHERE S.showID = E.showID;
+
+SELECT showName ,episodeTitle
+FROM Show S  
+JOIN Episode EP ON  EP.showID = S.showID 
+WHERE episodeDate LIKE '2024-12-__%';
+
+-- -- UPDATE  Timeslot 
+-- -- SET endtime = '19:30'
+-- -- WHERE episodeDate = '2024-12-24' AND showName = 'Star Cooks';
+
+-- SELECT showName AS 'ShowName', COUNT(ratingValue) AS 'TotalRatings'
+-- FROM Show S , Episode E, ViewerRating VR
+-- WHERE S.showID = E.showID
+-- AND E.episodeID = VR.episodeID
+-- GROUP BY showName
+-- ORDER BY  VR.episodeID ASC;
---
+++ starte.py
@@ -0,0 +1,26 @@
+def readData():
+    textinput=""
+    return textinput
+    
+def countWords(textinput):
+    count=0
+    return count 
+    
+def countVowels(textinput):
+    count=0
+    return count 
+
+def countConsonants(textinput):
+    count=0
+    return count
+
+def displayResults(words,vowels,consonants):
+    pass
+
+textinput = readData()
+words = countWords(textinput)
+vowels = countVowels(textinput)
+consonants = countConsonants(textinput)
+displayResults(words,vowels,consonants)
+    
+ 
