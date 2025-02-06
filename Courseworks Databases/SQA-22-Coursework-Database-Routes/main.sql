fb3039fb5bc7acf31b7fba928adfca500ceaddf2
NNortey15 <nor107@stge.org.uk>
2025-02-06 14:23:36 +0000

    Almost done databases

--- Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
+++ Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
@@ -1,1 +1,27 @@
 DESCRIBE ALL TABLES;
+
+-- SELECT  P.forename,P.surname,P.plannerNo, COUNT(W.walkID) AS "Total participants"
+-- FROM   Walk W, Route R, Planner P 
+-- WHERE W.routeID = R.routeID AND R.plannerNo =P.plannerNo
+-- GROUP BY P.forename,
+-- P.surname, P.plannerNo
+-- ORDER BY "Total participants" DESC; 
+
+-- SELECT W.walkerNo, W.forename,W.surname,W.telNo
+-- FROM Walker W, Route R, Walk WK
+-- WHERE 
+--     W.WalkerNo = WK.WalkerNo 
+--     AND Wk.routeID = R.routeID
+--     AND R.distance = 
+--         (
+--             SELECT MAX(distance)
+--             FROM Route AS "Longest"
+--         )
+-- GROUP BY W.telNo
+-- ORDER BY W.walkerNo ASC;
+
+SELECT Route.routeID, woodName, description
+FROM Route
+WHERE footwear = "Trail shoes"
+OR footwear = "Waterproof shoes"
+OR footwear = "Walking shoes";
