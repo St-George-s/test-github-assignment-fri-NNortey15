29c8cea5ff2606114e1cf4c72d2dda079954161d
NNortey15 <nor107@stge.org.uk>
2025-02-24 12:05:09 +0000

    CW 2025

--- CourseWork/2019/main.py
+++ CourseWork/2019/main.py
@@ -34,6 +34,52 @@ print("The furthest distance walked is: " + str(furthest))
 # Write club prize winners to file
 # Open results.txt file
 with open('results.txt', 'w') as file:
+#Find the position of the customer who gave the first 5-star rating in a given month.
+#def customerPosition(Orders):
+
+# 2.1	Set position to -1
+#orders = -1
+# 2.2	Set index to 0
+#index = 0
+# 2.3	Ask user to enter month to search for
+#UsersChosenMonth = (" Enter month to search for")
+# 2.4	While position is -1 and index is less than the length of the array
+#for x in range (1, len(Orders)
+# 2.5	    If current month is equal to searched month and current rating is 5 then
+#if orders[x].date and orders[x].rating == int(5):
+# 2.6	        Set position to index
+#orders = index
+# 2.7	    End if
+
+# 2.8	    Add 1 to index
+ #index = index + 1
+# 2.9	End while
+
+# 2.10	Return position
+#	return orders
+    
+# 3.1	Open new file ‘winningCustomer.txt’
+# with open("winningCustomer,txt", 'w') as file:
+        
+# 3.2	    If position is 0 or above then
+#if index >= 0 ;
+# 3.3	        Write winning order number, email and cost to ‘winningCustomer.txt’
+#file.write(Orders.orderNum + "" + Orders.email + Orders.cost + "\n")
+# 3.4	    Else
+# 3.5	        Write ‘No winner’ to ‘winningCustomer.txt’
+#file.write("No winner\n")
+# 3.6	    End if
+# 3.7	Close ‘winningCustomer.txt’
+#file.close()
+	
+# 4.1	Call countOption function to return the number of orders delivered
+# 4.2	Call countOption function to return the number of orders collected
+# 4.3	Output the total number of orders delivered
+# 4.4	Output the total number of orders collected
+
+
+
+
 
 #Write "The prize winning members are:" to the results.txt file
     file.write("The prize winning members are:\n")
