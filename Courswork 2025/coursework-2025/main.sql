947e9b471cd08b71b92769817d90894f0513a7be
NNortey15 <nor107@stge.org.uk>
2025-03-04 10:25:18 +0000

    CW databases

--- Courswork 2025/coursework-2025/main.sql
+++ Courswork 2025/coursework-2025/main.sql
@@ -1,1 +1,11 @@
 DESCRIBE ALL TABLES;
+
+-- SELECT comicTitle, issue , publisherName, valuation
+-- FROM Comic C ,publisher P;
+-- WHERE  valuation > 300
+-- GROUP BY comicTitle; 
+
+
+SELECT characterName, COUNT(valuation) + 300 AS 'Total Valuation'
+FROM CharacterInfo CI ,Comic C;
+WHERE characterName == %Duck%; 
