a66512837554b47471357533195923ee43977e8f
NNortey15 <nor107@stge.org.uk>
2025-03-06 16:27:49 +0000

    CW

--- Courswork 2025/coursework-2025/main.sql
+++ Courswork 2025/coursework-2025/main.sql
@@ -1,41 +1,29 @@
 
 
--- SELECT comicTitle, issue , publisherName ,valuation
--- FROM Comic C
--- JOIN Publisher P ON P.publisherID = C.publisherID
--- WHERE valuation >= (
+SELECT comicTitle, issue , publisherName ,valuation
+FROM Comic C
+JOIN Publisher P ON P.publisherID = C.publisherID
+WHERE valuation >= (
 
---         SELECT AVG(valuation) 
---         FROM Comic
---         )+ 300;
-
-
--- SELECT characterName, SUM(Valuation) AS "Total Valuation"
--- FROM CharacterInfo CI ,Comic C ,ComicCharacter CC
--- WHERE characterName LIKE "%Duck%" ;
+        SELECT AVG(valuation) 
+        FROM Comic
+        )+ 300;
 
 
--- SELECT comicTitle, issue, publisherName,(valuation * 2 ) AS "Double Price"
--- FROM Comic C , Series S ,CharacterInfo CI , ComicCharacter CC 
--- Join Publisher P ON C.publisherID = P.publisherID
--- JOIN Series S ON C.seriesID = S.seriesID
--- JOIN ComicCharacter CC ON CI.characterID = CC.characterID
--- WHERE characterName = "Starlodly"
---  AND seriesName = (  SELECT seriesName
---                      FROM Series 
---                      WHERE seriesName = "The OK seven");
 
--- SELECT comicTitle,startYear,seriesName
--- FROM Series,Comic
--- WHERE startYear = "1980";
+SELECT characterName, SUM(valuation) AS TotalValuation 
+FROM CharacterInfo CI
+JOIN ComicCharacter CC ON CI.characterID = CC.characterID
+ JOIN Comic C  ON CC.comicID = C.comicID 
+ WHERE characterName LIKE '%Duck%'
+  GROUP BY characterName 
+  ORDER BY TotalValuation DESC;
 
-
-SELECT comicTitle , issue, publisherName , (valuation * 2) AS "Double Price"
-FROM Comic , Series , CharacterInfo , ComicCharacter
-JOIN Publisher ON Comic.publisherID = Publisher.publisherID
+SELECT comicTitle, issue, publisherName, (valuation * 2) AS "Double Price"
+FROM Comic 
+JOIN Publisher ON Comic.publisherID = Publisher.publisherID 
 JOIN Series ON Comic.seriesID = Series.seriesID
-JOIN ComicCharacter ON CharacterInfo.characterID = ComicCharacter.characterID
-WHERE characterName = "Starlodly"
- AND seriesName = (  SELECT seriesName
-                     FROM Series 
-                     WHERE seriesName = "The OK seven");
+ JOIN ComicCharacter ON Comic.comicID = ComicCharacter.comicID 
+JOIN CharacterInfo ON ComicCharacter.characterID = CharacterInfo.characterID 
+WHERE Series.seriesName = 'The OK Seven' 
+AND characterName = 'Starlordly';
