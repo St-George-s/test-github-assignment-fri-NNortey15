f1a8dd7f9b6283c69cd790d2c2db588668af3ade
NNortey15 <nor107@stge.org.uk>
2025-03-06 14:20:45 +0000

    CW

--- Courswork 2025/coursework-2025/main.sql
+++ Courswork 2025/coursework-2025/main.sql
@@ -1,14 +1,41 @@
 
 
-SELECT comicTitle, issue , publisherName ,valuation
-FROM Comic C ,publisher P
-WHERE valuation >= (
+-- SELECT comicTitle, issue , publisherName ,valuation
+-- FROM Comic C
+-- JOIN Publisher P ON P.publisherID = C.publisherID
+-- WHERE valuation >= (
 
-        SELECT AVG(valuation) + 300
-        FROM Comic
-        )
-GROUP BY ;
+--         SELECT AVG(valuation) 
+--         FROM Comic
+--         )+ 300;
 
--- SELECT characterName, COUNT(valuation) + 300 AS 'Total Valuation'
--- FROM CharacterInfo CI ,Comic C;
--- WHERE characterName == %Duck%; 
+
+-- SELECT characterName, SUM(Valuation) AS "Total Valuation"
+-- FROM CharacterInfo CI ,Comic C ,ComicCharacter CC
+-- WHERE characterName LIKE "%Duck%" ;
+
+
+-- SELECT comicTitle, issue, publisherName,(valuation * 2 ) AS "Double Price"
+-- FROM Comic C , Series S ,CharacterInfo CI , ComicCharacter CC 
+-- Join Publisher P ON C.publisherID = P.publisherID
+-- JOIN Series S ON C.seriesID = S.seriesID
+-- JOIN ComicCharacter CC ON CI.characterID = CC.characterID
+-- WHERE characterName = "Starlodly"
+--  AND seriesName = (  SELECT seriesName
+--                      FROM Series 
+--                      WHERE seriesName = "The OK seven");
+
+-- SELECT comicTitle,startYear,seriesName
+-- FROM Series,Comic
+-- WHERE startYear = "1980";
+
+
+SELECT comicTitle , issue, publisherName , (valuation * 2) AS "Double Price"
+FROM Comic , Series , CharacterInfo , ComicCharacter
+JOIN Publisher ON Comic.publisherID = Publisher.publisherID
+JOIN Series ON Comic.seriesID = Series.seriesID
+JOIN ComicCharacter ON CharacterInfo.characterID = ComicCharacter.characterID
+WHERE characterName = "Starlodly"
+ AND seriesName = (  SELECT seriesName
+                     FROM Series 
+                     WHERE seriesName = "The OK seven");
